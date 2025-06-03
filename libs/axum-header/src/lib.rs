24ed633773b20ac3eb803f696c62f89cd2930fcd
HellLord <ratul.debnath.year@gmail.com>
2025-06-03 23:10:44 +0600

    remove OptionalHeader

--- libs/axum-header/src/lib.rs
+++ libs/axum-header/src/lib.rs
@@ -2,13 +2,7 @@ mod error;
 mod header;
 mod rejection;
 
-#[cfg(feature = "extra")]
-mod extra;
-
-pub use crate::error::TryIntoHeaderError;
-pub use crate::error::TryIntoHeaderErrorKind;
-pub use crate::header::Header;
-pub use crate::rejection::HeaderRejection;
-
-#[cfg(feature = "extra")]
-pub use crate::extra::OptionalHeader;
+pub use error::TryIntoHeaderError;
+pub use error::TryIntoHeaderErrorKind;
+pub use header::Header;
+pub use rejection::HeaderRejection;
--- libs/axum-header/tests/extra_integration_test.rs
+++ libs/axum-header/tests/extra_integration_test.rs
@@ -4,7 +4,6 @@ use axum::Router;
 use axum::routing::get;
 use axum::routing::post;
 use axum_header::Header;
-use axum_header::OptionalHeader;
 use axum_test::TestServer;
 use http::StatusCode;
 use serde::Deserialize;
@@ -53,57 +52,4 @@ async fn correct_rejection_status_code() {
         res.text(),
         "Failed to deserialize header string: n: invalid digit found in string"
     );
-}
-
-#[tokio::test]
-async fn optional_header_supports_multiple_values() {
-    #[derive(Deserialize)]
-    struct Data {
-        #[serde(rename = "value")]
-        values: Vec<String>,
-    }
-
-    let app = Router::new().route(
-        "/",
-        post(|OptionalHeader(data): OptionalHeader<Data>| async move {
-            data.map(|Data { values }| values.join(","))
-                .unwrap_or("None".to_owned())
-        }),
-    );
-
-    let server = TestServer::new(app).unwrap();
-
-    let res = server
-        .post("/")
-        .add_header("value", "one")
-        .add_header("value", "two")
-        .await;
-
-    assert_eq!(res.status_code(), StatusCode::OK);
-    assert_eq!(res.text(), "one,two");
-}
-
-#[tokio::test]
-async fn optional_header_deserializes_no_values_into_none() {
-    #[derive(Deserialize)]
-    struct Data {
-        value: String,
-    }
-
-    let app = Router::new().route(
-        "/",
-        post(|OptionalHeader(data): OptionalHeader<Data>| async move {
-            match data {
-                None => "None".into(),
-                Some(data) => data.value,
-            }
-        }),
-    );
-
-    let server = TestServer::new(app).unwrap();
-
-    let res = server.post("/").await;
-
-    assert_eq!(res.status_code(), StatusCode::OK);
-    assert_eq!(res.text(), "None");
 }
