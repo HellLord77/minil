6481cad9308b7293ff2079f9a9af1dce118bd0eb
HellLord <ratul.debnath.year@gmail.com>
2025-06-27 15:28:44 +0600

    Add new S3 error response types and bump version
    Introduced new error response types: EncryptionTypeMismatch, InvalidWriteOffset, and TooManyParts, each with their own output structs and modules. Updated error module exports to include these new types. Bumped axum-s3 crate version to 0.3.2.

--- lib/axum-s3/src/error/mod.rs
+++ lib/axum-s3/src/error/mod.rs
@@ -1,11 +1,17 @@
 mod bucket_already_exists;
 mod bucket_already_owned_by_you;
 mod conditional_request_conflict;
+mod encryption_type_mismatch;
+mod invalid_write_offset;
 mod no_such_bucket;
 mod precondition_failed;
+mod too_many_parts;
 
 pub use bucket_already_exists::BucketAlreadyExistsOutput;
 pub use bucket_already_owned_by_you::BucketAlreadyOwnedByYouOutput;
 pub use conditional_request_conflict::ConditionalRequestConflictOutput;
+pub use encryption_type_mismatch::EncryptionTypeMismatchOutput;
+pub use invalid_write_offset::InvalidWriteOffsetOutput;
 pub use no_such_bucket::NoSuchBucketOutput;
 pub use precondition_failed::PreconditionFailedOutput;
+pub use too_many_parts::TooManyPartsOutput;
