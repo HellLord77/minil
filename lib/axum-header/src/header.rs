74c861d9409b9b33aec233c400c62485f0779035
HellLord <ratul.debnath.year@gmail.com>
2025-07-01 20:09:21 +0600

    Remove feature flags and multi header support from axum-header
    This commit removes the 'single' and 'multi' feature flags from axum-header, making serde_html_form the default and only deserializer. The related compile-time checks and conditional code are deleted, and the multi_integration_test.rs test is removed as multi-value header support is no longer configurable.

--- lib/axum-header/src/header.rs
+++ lib/axum-header/src/header.rs
@@ -34,9 +34,6 @@ where
             .finish();
 
         let parser = form_urlencoded::parse(encoded.as_bytes());
-        #[cfg(feature = "single")]
-        let deserializer = serde_urlencoded::Deserializer::new(parser);
-        #[cfg(feature = "multi")]
         let deserializer = serde_html_form::Deserializer::new(parser);
 
         Ok(Header(
@@ -62,9 +59,6 @@ where
 
     fn into_response_parts(self, mut res: ResponseParts) -> Result<ResponseParts, Self::Error> {
         let mut encoder = form_urlencoded::Serializer::new(String::new());
-        #[cfg(feature = "single")]
-        let serializer = serde_urlencoded::Serializer::new(&mut encoder);
-        #[cfg(feature = "multi")]
         let serializer = serde_html_form::Serializer::new(&mut encoder);
 
         self.0.serialize(serializer).unwrap_or_else(|_err| todo!());
--- lib/axum-header/src/lib.rs
+++ lib/axum-header/src/lib.rs
@@ -1,9 +1,3 @@
-#[cfg(any(
-    not(any(feature = "single", feature = "multi")),
-    all(feature = "single", feature = "multi")
-))]
-compile_error!("expected one of single or multi");
-
 mod error;
 mod header;
 mod rejection;
