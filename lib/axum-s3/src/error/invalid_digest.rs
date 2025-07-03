8b6b20fd9560b3d8daf1b214517c32263b40484e
HellLord <ratul.debnath.year@gmail.com>
2025-07-03 23:40:40 +0600

    Add InvalidDigest and NotImplemented error types
    Introduces new error types InvalidDigestOutput and NotImplementedOutput in the axum-s3 crate, with corresponding modules and exports. Bumps crate version to 0.3.8 to reflect these additions.

---
+++ lib/axum-s3/src/error/invalid_digest.rs
@@ -0,0 +1,15 @@
+use axum::http::StatusCode;
+use axum_into_response::IntoResponse;
+use axum_s3_macros::ErrorFromRequestParts;
+use axum_xml::Xml;
+use bon::Builder;
+use serde_s3::types::error::InvalidDigest;
+
+#[derive(Debug, Builder, IntoResponse, ErrorFromRequestParts)]
+pub struct InvalidDigestOutput {
+    #[builder(default = StatusCode::BAD_REQUEST)]
+    pub status: StatusCode,
+
+    #[into_response(via(Xml))]
+    pub body: InvalidDigest,
+}
---
+++ lib/axum-s3/src/error/not_implemented.rs
@@ -0,0 +1,15 @@
+use axum::http::StatusCode;
+use axum_into_response::IntoResponse;
+use axum_s3_macros::ErrorFromRequestParts;
+use axum_xml::Xml;
+use bon::Builder;
+use serde_s3::types::error::NotImplemented;
+
+#[derive(Debug, Builder, IntoResponse, ErrorFromRequestParts)]
+pub struct NotImplementedOutput {
+    #[builder(default = StatusCode::NOT_IMPLEMENTED)]
+    pub status: StatusCode,
+
+    #[into_response(via(Xml))]
+    pub body: NotImplemented,
+}
