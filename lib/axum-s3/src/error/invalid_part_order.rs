e87fd9384a72cf89015995a91d9cd4890522ecdd
HellLord <ratul.debnath.year@gmail.com>
2025-07-22 17:33:28 +0600

    Add InvalidPart and InvalidPartOrder error types
    Introduces new error types InvalidPartOutput and InvalidPartOrderOutput to handle specific S3 error responses. Updates the error module to include these new types and bumps the crate version to 0.4.5.

---
+++ lib/axum-s3/src/error/invalid_part_order.rs
@@ -0,0 +1,15 @@
+use axum::http::StatusCode;
+use axum_derive_macros::IntoResponse;
+use axum_s3_macros::ErrorFromParts;
+use axum_serde::Xml;
+use bon::Builder;
+use serde_s3::types::error::InvalidPartOrder;
+
+#[derive(Debug, Builder, IntoResponse, ErrorFromParts)]
+pub struct InvalidPartOrderOutput {
+    #[builder(default = StatusCode::BAD_REQUEST)]
+    pub status: StatusCode,
+
+    #[into_response(via(Xml))]
+    pub body: InvalidPartOrder,
+}
