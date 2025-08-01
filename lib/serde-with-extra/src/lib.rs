bf13560613b30e79b9062ae80caac873fe3a1e5f
HellLord <ratul.debnath.year@gmail.com>
2025-08-01 17:42:15 +0600

    Remove deprecated AsString and bump version
    Deleted the deprecated AsString implementation from serde-with-extra and removed its export from lib.rs. Bumped crate version to 0.2.1 to reflect the change.

--- lib/serde-with-extra/src/lib.rs
+++ lib/serde-with-extra/src/lib.rs
@@ -1,10 +1,8 @@
-mod as_string;
 mod display_from_bytes;
 
 #[cfg(feature = "http-range")]
 mod http_range;
 
-pub use as_string::AsString;
 pub use display_from_bytes::DisplayFromBytes;
 #[cfg(feature = "http-range")]
 pub use http_range::SerdeHttpRange;
